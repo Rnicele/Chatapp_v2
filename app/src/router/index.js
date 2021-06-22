cabf66a5e0e7c1f1c088ded1c89e7dc8335c2c85
Rnikele <nicele.reyes26@gmail.com>
2021-06-22 17:05:55 +0800

    installed vuetify and vue form validation. UI for login and signup is done

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,22 +1,33 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+// import Home from '../views/Home.vue'
+import SignUp from '../views/Signup.vue'
+import Signin from '../views/Signin.vue'
 Vue.use(VueRouter)
 
 const routes = [
+  // {
+  //   path: '/',
+  //   name: 'Home',
+  //   component: Home
+  // },
+  // {
+  //   path: '/about',
+  //   name: 'About',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  // },
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/Signup',
+    name: 'Signup',
+    component: SignUp
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Signin',
+    name: 'Signin',
+    component: Signin
   }
 ]
 
---
+++ app/src/views/Signup.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-container fill-height fluid>
+        <v-row align="center" justify="center">
+            <v-card width="700" class="mx-auto">
+                <v-card-title>SIGN UP</v-card-title>
+                <v-divider></v-divider>
+                <v-form 
+                    ref="form"
+                    v-model="valid"
+                    lazy-validation style="margin: 20px 30px 0 30px;"
+                >
+                    <v-text-field outlined
+                    v-model="first_name"
+                    label="First Name"
+                    :rules="[v => !!v || 'First Name is required']"
+                    required
+                    ></v-text-field>
+
+                    <v-text-field outlined
+                    v-model="last_name"
+                    label="Last Name"
+                    :rules="[v => !!v || 'Last Name is required']"
+                    required
+                    ></v-text-field>
+
+                    <v-text-field outlined
+                    v-model="username"
+                    label="Username"
+                    :rules="[v => !!v || 'Username is required']"
+                    required
+                    ></v-text-field>
+
+                    <v-text-field outlined
+                    v-model="email"
+                    label="E-mail"
+                    :rules="[v => !!v || 'Email is required']"
+                    required
+                    ></v-text-field>
+
+                    <v-text-field outlined
+                    v-model="password1"
+                    type="password"
+                    label="Password"
+                    :rules="[v => !!v || 'Password is required']"
+                    required
+                    ></v-text-field>
+
+                    <v-text-field outlined
+                    v-model="password2"
+                    type="password"
+                    label="Confirm Password"
+                    :rules="[v => !!v || 'Confirm Password is required']"
+                    required
+                    ></v-text-field>
+
+                    <v-card-actions>
+                        <v-btn class="mr-4">
+                        Signup
+                        </v-btn>
+                    </v-card-actions>
+                    
+                    <v-card-text style="padding-left:0px">
+                        Already have an account? <a href="#/Signin"> Log in </a>
+                    </v-card-text>
+                </v-form>
+                
+            </v-card>
+        </v-row>
+     </v-container>
+</template>
