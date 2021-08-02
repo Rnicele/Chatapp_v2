300357072a44f86a22e41924dad96da17adfc1ea
Rnikele <nicele.reyes26@gmail.com>
2021-08-02 11:47:13 +0800

    allow_null true for avatar

--- api/chat/serializers.py
+++ api/chat/serializers.py
@@ -18,7 +18,7 @@ class RegisterSerializer(serializers.Serializer):
     email = serializers.EmailField()
     username = serializers.CharField(max_length=255)
     password = serializers.CharField(max_length=255)
-    avatar = serializers.CharField(max_length=255)
+    avatar = serializers.CharField(max_length=255, allow_null=True)
 
     def register(self):
         user = User(**self.validated_data)
